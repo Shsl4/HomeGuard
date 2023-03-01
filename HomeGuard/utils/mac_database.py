075dc5a6c654025147eb41c8fdaaaef0359cbd69
Sl4sh! <pierre.juliot@proton.me>
2023-03-01 18:32:25 +0100

    Update mac_database.py

--- HomeGuard/utils/mac_database.py
+++ HomeGuard/utils/mac_database.py
@@ -5,7 +5,7 @@ class MacDatabase:
 
     def __init__(self):
         self.data = {}
-        with(open('resources/mac_addresses.csv')) as csvfile:
+        with(open('resources/mac_addresses.csv', encoding="utf8")) as csvfile:
             reader = csv.reader(csvfile, delimiter=',')
             for row in reader:
                 self.data[row[0]] = row[1]
